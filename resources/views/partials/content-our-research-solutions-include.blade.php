577bf87bdc74e4676127d0778a2fd09751c83979
Ken Tsang <ken@mediachain.co>
2019-06-13 17:38:24 +0100

    navigation

--- resources/views/partials/content-our-research-solutions-include.blade.php
+++ resources/views/partials/content-our-research-solutions-include.blade.php
@@ -1,11 +1,10 @@
-<section class="wrap how-we-do-it">
+<section class="wrap white-section">
         <div class="container row">
-            <div class="animated-svg">
+                <div class="content">
+            <h2 class="alt">Our research solutions include:</h2>
+                </div>
             </div>
-            <div class="content">
-                <h2>Discover what <span>matters</span> on social</h2>
-                <p>social-first data is changing the way brands approach campaigns. in a landscape that's already overcrowded, insights cut through to make messages stand out</p>
-                <div class=""></div>
-            </div>
+            <div class="tools">
+
             </div>
         </section>
--- resources/views/partials/content-our-tools-and-research.blade.php
+++ resources/views/partials/content-our-tools-and-research.blade.php
@@ -1,11 +1,16 @@
-<section class="wrap how-we-do-it">
-        <div class="container row">
-            <div class="animated-svg">
+<section class="wrap grey-section">
+        <div class="container column">
+                <div class="content-alt">
+            <h2 class="alt">Our tools and resources</h2>
+                </div>
+            <div class="tools">
+                    <div class="resources">1</div>
+                    <div class="resources">2</div>
+                    <div class="resources">3</div>
+                    <div class="resources">4</div>
+                    <div class="resources">5</div>
+                    <div class="resources">6</div>
+                    <div class="resources">7</div>
             </div>
-            <div class="content">
-                <h2>Discover what <span>matters</span> on social</h2>
-                <p>social-first data is changing the way brands approach campaigns. in a landscape that's already overcrowded, insights cut through to make messages stand out</p>
-                <div class=""></div>
-            </div>
-            </div>
+        </div>
         </section>
