6d856328bb696ee6a004a780fabb4600ff0a2595
Ken Tsang <ken@mediachain.co>
2019-06-14 13:33:07 +0100

    updates

--- resources/views/partials/content-our-tools-and-research.blade.php
+++ resources/views/partials/content-our-tools-and-research.blade.php
@@ -4,13 +4,34 @@
             <h2 class="alt">Our tools and resources</h2>
                 </div>
             <div class="tools">
-                    <div class="resources">1</div>
-                    <div class="resources">2</div>
-                    <div class="resources">3</div>
-                    <div class="resources">4</div>
-                    <div class="resources">5</div>
-                    <div class="resources">6</div>
-                    <div class="resources">7</div>
+                    <div class="resources">
+                            <img src="@asset('images/png/crimson-hexagon.png')"/>
+                        <div class="resources__title">Crimson Hexagon</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/global-web-index.png')"/>
+                        <div class="resources__title">Global Web Index</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/julius.png')"/>
+                        <div class="resources__title">Julius</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/tagger.png')"/>
+                        <div class="resources__title">Tagger</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/brand24.png')"/>
+                        <div class="resources__title">Brand24</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/google-trends.png')"/>
+                        <div class="resources__title">Google Trends</div>
+                    </div>
+                    <div class="resources">
+                            <img src="@asset('images/png/facebook.png')"/>
+                        <div class="resources__title">Facebook Audience Insights</div>
+                    </div>
             </div>
         </div>
         </section>
