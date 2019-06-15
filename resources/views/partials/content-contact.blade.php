e78839e3d8ea9a22c6295b6987c6d2343cd176ae
Ken Tsang <ken@mediachain.co>
2019-06-15 21:05:38 +0100

    update

--- resources/views/partials/content-contact.blade.php
+++ resources/views/partials/content-contact.blade.php
@@ -1,8 +1,18 @@
 <section class="wrap white-section">
         <div class="container row">
-            <div class="animated-svg">
+            <div class="content">
+                <div class="telephone"><a href="tel:01619747887">0161 974 7887</a></div>
+                <div class="email"><a href="mailto:data@socialchain.com">data@socialchain.com</a></div>
+                <div class="social">
+                        <a href="#" class="social__icons"><img src="@asset('images/svg/instagram.svg')"/></a>
+                        <a href="#" class="social__icons"><img src="@asset('images/svg/twitter.svg')"/></a>
+                        <a href="#" class="social__icons"><img src="@asset('images/svg/facebook.svg')"/></a>
+                </div>
+                <div class="fbmessenegr">
+                        <img src="@asset('images/svg/Facebook_Messenger_logo.svg')"/>
+                    </div>
             </div>
-            <div class="content contact-form">
+            <div class="contact-form">
                 {{ gravity_form( 2, false, false ) }}
             </div>
             </div>
