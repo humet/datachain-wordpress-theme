fa78786c162bc1c0fdf1f94a703db3d79bb58bd0
Ken Tsang <ken@mediachain.co>
2019-06-16 20:59:09 +0100

    more updates on mobile tylings

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -4,5 +4,10 @@ export default {
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
+
+    $('.mobile-nav-button').click(function() {
+      $('#nav-icon2').toggleClass('open');
+      $('.mobile-nav-overlay').toggleClass('show');
+  });
   },
 };
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,22 @@
 <header class="wrap header @if (!is_front_page()) grey border-bottom @endif ">
   <div class="container">
     <a class="brand" href="{{ home_url('/') }}"><img src="@asset('images/svg/social-chain-data.svg')"/></a>
+    <div class="mobile-nav-button">
+      <div id="nav-icon2">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  <div class="mobile-nav-overlay">
     <nav class="nav-primary">
       @if (has_nav_menu('primary_navigation'))
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
+  </div>
   </div>
 </header>
