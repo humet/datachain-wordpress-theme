72b3362fe503f2d59b0bd2dcc101a492a9a74544
Ken Tsang <ken@mediachain.co>
2019-07-24 14:37:15 +0100

    videos

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,5 +1,12 @@
 <div class="video-overlay">
-  hello
+  <div class="cross"></div>
+  <div itemscope itemtype="https://schema.org/VideoObject">
+    <meta itemprop="uploadDate" content="Wed Jul 24 2019 14:08:37 GMT+0100 (British Summer Time)"/>
+    <meta itemprop="name" content="Sc-data-2407"/>
+    <meta itemprop="duration" content="PT2M28.89S" />
+    <meta itemprop="thumbnailUrl" content="https://content.jwplatform.com/thumbs/ZiwuPADI-1280.jpg"/>
+    <meta itemprop="contentUrl" content="https://content.jwplatform.com/videos/ZiwuPADI-WoqsQSXj.mp4"/>
+    <script src="https://cdn.jwplayer.com/players/ZiwuPADI-T0PF2eBM.js"></script></div>
 </div>
 <footer class="wrap footer">
     <div class="footer-top">
