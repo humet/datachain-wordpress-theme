5b17791b8d319476102267237ae802141437b455
Ken Tsang <ken@mediachain.co>
2019-07-25 16:09:00 +0100

    updater chatbot

--- resources/views/partials/chatbot.blade.php
+++ resources/views/partials/chatbot.blade.php
@@ -1,19 +1,6 @@
-<div class="chat-bot">
-  <div class="hand">
-      <img class="animated infinite tada slow" src="@asset('images/svg/hand.svg')" />
-  </div>
-  <div class="close"></div>
-</div>
-<div class="widget">
-  <div class="widget-area">
-    <script src="https://static.landbot.io/landbot-widget/landbot-widget-1.0.0.js"></script>
-    <div id="myLandbot" style="width: 100%; height: 500px"></div>
-    <script>
-      var myLandbot = new LandbotFrameWidget({
-        container: '#myLandbot',
-        index: 'https://landbot.io/u/H-210125-BKMTLHJ7HILMQ0RM/index.html',
-      });
-    </script>
-  </div>
-  <div class="close-widget"></div>
-</div>
+<script src="https://static.landbot.io/landbot-3/landbot-3.js"></script>
+<script>
+  var myLandbot = new Landbot.Livechat({
+    configUrl: 'https://landbot.io/u/H-210125-BKMTLHJ7HILMQ0RM/index.json',
+  });
+</script>
